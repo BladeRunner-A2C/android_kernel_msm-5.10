1618e2f1ceb18eda9a5b4a48ecd3ed3dc5a81e3a
BladeRunner-A2C <creator4983@aospa.co>
2026-06-07 10:03:53 +0200

    Add a script to merge CLO tag in all submodules
    This is the commit message #2:
    
    merge-tag.py: Always merge actual revision instead of tag
    
    Parse vendor manifest for all repos, to avoid the case when qcom derps
    by mismatching tag and revision (happened in some rare cases). Also
    improve commit messages.
    
    This is the commit message #3:
    
    merge-tag.py: Obtain a fallback revision from user
    
    In some rare cases CLO literally forgets to push a manifest xml.
    
    This is the commit message #4:
    
    merge-tag.py: reformat
    
    This is the commit message #5:
    
    merge-tag.py: adapt for in kernel techpack repos
    
    Co-authored-by: Adithya R <gh0strider.2k18.reborn@gmail.com>
    Change-Id: I5f5b3637f3eab35f5130f9fe543cc2f80630996f

---
+++ merge-tag.py
@@ -0,0 +1,158 @@
+#!/usr/bin/env python3
+#
+# SPDX-FileCopyrightText: Adithya R
+# SPDX-License-Identifier: MIT
+#
+
+import subprocess
+import sys
+import xml.etree.ElementTree as ET
+
+import requests
+
+if len(sys.argv) != 2:
+    print('Usage:\n\t./merge-tag.py <tag>')
+    sys.exit(1)
+
+tag = sys.argv[1]
+
+modules = {
+    'fw-api': 'https://git.codelinaro.org/clo/la/platform/vendor/qcom-opensource/wlan/fw-api',
+    'qca-wifi-host-cmn': 'https://git.codelinaro.org/clo/la/platform/vendor/qcom-opensource/wlan/qca-wifi-host-cmn',
+    'qcacld-3.0': 'https://git.codelinaro.org/clo/la/platform/vendor/qcom-opensource/wlan/qcacld-3.0',
+    'audio-kernel': 'https://git.codelinaro.org/clo/la/platform/vendor/qcom/opensource/audio-kernel-ar',
+    'camera-kernel': 'https://git.codelinaro.org/clo/la/platform/vendor/opensource/camera-kernel',
+    'cvp-kernel': 'https://git.codelinaro.org/clo/la/platform/vendor/opensource/cvp-kernel',
+    'dataipa': 'https://git.codelinaro.org/clo/la/platform/vendor/opensource/dataipa',
+    'datarmnet': 'https://git.codelinaro.org/clo/la/platform/vendor/qcom/opensource/datarmnet',
+    'datarmnet-ext': 'https://git.codelinaro.org/clo/la/platform/vendor/qcom/opensource/datarmnet-ext',
+    'display-drivers': 'https://git.codelinaro.org/clo/la/platform/vendor/opensource/display-drivers',
+    'eva-kernel': 'https://git.codelinaro.org/clo/la/platform/vendor/opensource/eva-kernel',
+    'mmrm-driver': 'https://git.codelinaro.org/clo/la/platform/vendor/opensource/mmrm-driver',
+    'video-driver': 'https://git.codelinaro.org/clo/la/platform/vendor/opensource/video-driver',
+}
+
+# These are in a separate manifest
+separated_techpacks = {
+    'camera-kernel': 'camera',
+    'display-drivers': 'display',
+    'mmrm-driver': 'video',
+    'video-driver': 'video',
+}
+
+
+# Input revision from user - fallback
+# for that facepalm moment when they forget to release a manifest xml
+def get_revision_from_user():
+    i = input('Enter custom tag or revision to proceed, or s to skip: ')
+    return None if i.casefold() == 's' else i
+
+
+# Parses the module revision from a vendor manifest
+def get_revision_from_manifest(tag, path, techpack=None):
+    if techpack is None:
+        root = manifest_root
+    else:
+        response = requests.get(
+            f'https://git.codelinaro.org/clo/la/techpack/{techpack}/manifest/-/raw/release/{tag}.xml'
+        )
+        if response.status_code != 200:
+            print('Failed to load', techpack, 'manifest!')
+            return get_revision_from_user()
+        root = ET.fromstring(response.content)
+
+    for project in root.findall('project'):
+        repo_path = project.get('path')
+        if repo_path is not None and repo_path.endswith(path):
+            return project.get('revision')
+
+    if techpack is None:
+        print('Failed to obtain revision from vendor manifest!')
+    else:
+        print('Failed to obtain revision from', techpack, 'techpack manifest!')
+
+    return get_revision_from_user()
+
+
+# Obtains the tag for the given techpack manifest from vendor manifest
+def get_techpack_tag(techpack):
+    refs = manifest_root.find('refs')
+    if refs is not None:
+        for image in refs:
+            project = image.get('project')
+            if project == 'techpack/' + techpack + '/manifest':
+                return image.get('tag')
+
+    print('Failed to obtain', techpack, 'techpack tag from manifest!')
+
+
+# Returns (actual_revision, display_revision)
+def get_revision(path):
+    if path in separated_techpacks:
+        techpack = separated_techpacks.get(path)
+        tp_tag = get_techpack_tag(techpack)
+        if tp_tag is not None:
+            print('Techpack tag:', tp_tag)
+            return (get_revision_from_manifest(tp_tag, path, techpack), tp_tag)
+    else:
+        return (get_revision_from_manifest(tag, path), tag)
+
+
+# HERE IT BEGINS
+response = requests.get(
+    f'https://git.codelinaro.org/clo/la/la/vendor/manifest/-/raw/release/{tag}.xml'
+)
+if response.status_code != 200:
+    print('Tag does not exist!')
+    sys.exit(1)
+
+# print(response.content)
+manifest_root = ET.fromstring(response.content)
+
+for path, url in modules.items():
+    print('\nMerging', path)
+
+    revs = get_revision(path)
+    if revs is None:
+        print('Failed to obtain revision, bailing!')
+        continue
+
+    rev, display_rev = revs
+    if rev is None:
+        print('No revision obtained, skipping!')
+        continue
+
+    print('URL:', url)
+    print('Revision:', rev)
+
+    if path in ['fw-api', 'qca-wifi-host-cmn', 'qcacld-3.0']:
+        commit_msg = f"{path.replace('/', ': ')}: Merge tag '{display_rev}'"
+        subtree = '-Xsubtree=drivers/staging/' + path
+    else:
+        commit_msg = (
+            f"techpack: {path.replace('/', ': ')}: Merge tag '{display_rev}'"
+        )
+        subtree = '-Xsubtree=techpack/' + path
+
+    if display_rev != tag:
+        commit_msg += f"\n\nFrom vendor tag: '{tag}'"
+
+    try:
+        subprocess.run(['git', 'fetch', url, rev], check=True)
+        subprocess.run(
+            [
+                'git',
+                'merge',
+                'FETCH_HEAD',
+                subtree,
+                '-m',
+                commit_msg,
+                '--log=2147483647',
+            ],
+            check=True,
+        )
+    except subprocess.CalledProcessError:
+        print('Failed to merge!')
+        break
+
+    print('Succesfully merged!')
